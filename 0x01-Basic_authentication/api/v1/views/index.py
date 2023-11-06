52934a711d98fa6fbd3024d75c2b0b54364fd229
Gibexz <andrewetedu@gmail.com>
2023-11-07 00:28:59 +0100

    Task 1. Error handler: Unauthorized implementation v1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized():
     """
     for the 401 error handler
