f662d3c0edd36be3229edfe1b2a966b9dbeeeaca
Gibexz <andrewetedu@gmail.com>
2023-11-07 22:05:44 +0100

    Task 10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 module basic_auth.py
 """
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 import base64
 
 
@@ -56,3 +58,24 @@ class BasicAuth(Auth):
             return email, password
         else:
             return None, None
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        returns the User instance based on his email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        if not User.all():
+            return None
+
+        matching_users = User.search({"email": user_email})
+        if not matching_users:
+            return None
+        else:
+            for user in matching_users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
