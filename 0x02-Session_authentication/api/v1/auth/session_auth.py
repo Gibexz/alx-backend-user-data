c73b06f8b96752a6b470eb067d529bc3d67144d6
Gibexz <andrewetedu@gmail.com>
2023-11-08 21:17:18 +0100

    Task 1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+module: session_auth.py
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    session authenctication class
+    empty for now
+    """
