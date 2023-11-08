02ebe525e75ee20c0d94d4eea5f7aa5673070a3b
Gibexz <andrewetedu@gmail.com>
2023-11-08 22:37:30 +0100

    Task 5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,11 +58,13 @@ def before_request():
     """
     allowed_path_list = ['/api/v1/status/',
                          '/api/v1/unauthorized/',
-                         '/api/v1/forbidden/']
+                         '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
     if auth is None:
         return
     if auth.require_auth(request.path, allowed_path_list):
-        if auth.authorization_header(request) is None:
+        if (auth.authorization_header(request) is None and
+                auth.session_cookie(request) is None):
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
