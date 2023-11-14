dc0b4f5806594cf09a1415e86df4ea9cffb22fb3
Gibexz <andrewetedu@gmail.com>
2023-11-15 00:32:26 +0100

    Task 15. User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,5 +65,18 @@ def logout():
         abort(403)
 
 
+@app.route('/profile', methods=["GET"], strict_slashes=False)
+def profile():
+    """displays a user profile as a JSON payload"""
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+        else:
+            abort(403)
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
