e9f59d2508354eee9cdb4d81f3f673e4b5262d79
Gibexz <andrewetedu@gmail.com>
2023-11-14 22:07:35 +0100

    Task updates and Task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from db import DB
 from typing import TypeVar
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
+from uuid import uuid4
 
 
 def _hash_password(rawPassword: str) -> bytes:
@@ -16,6 +17,11 @@ def _hash_password(rawPassword: str) -> bytes:
     hasedPass = bcrypt.hashpw(rawPassword.encode('utf-8'), bcrypt.gensalt())
 
     return hasedPass
+
+
+def _generate_uuid() -> str:
+    """uuid string generator"""
+    return (uuid4())
 
 
 class Auth:
@@ -50,3 +56,18 @@ class Auth:
                     return False
         except Exception as e:
             return False
+
+    def create_session(self, email: str) -> str:
+        """session generator method"""
+        try:
+            user = self._db.find_user_by(email=email)
+            # print (str(user))
+            sessionID = str(_generate_uuid())
+            # try using update_user form _db below if this fails.
+            # self._db.update_user(user.id, session_id=sessionID)
+            user.session_id = sessionID
+            self._db._session.commit()
+            return (sessionID)
+
+        except Exception as e:
+            return None
